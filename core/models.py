c9f5a501b1593d0c18331059a10308ed1c390ea8
Jhair Ante <jhair173@gmail.com>
2024-06-28 14:57:10 -0600

    model correction

--- core/models.py
+++ core/models.py
@@ -1,68 +1,50 @@
 from django.db import models
 
-class Usuario(models.Model):
-    id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=50)
-    apellidos = models.CharField(max_length=100)
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+)
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+# Create your models here.
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=100)
     username = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=128)
+    email = models.EmailField(max_length=255, unique=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f"{self.nombre} {self.apellidos}"
-
-    class Meta:
-        db_table = 'usuarios'
-
-
-class Categoria(models.Model):
-    id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.nombre
-
-    class Meta:
-        db_table = 'categorias'
-
-
-class Grupo(models.Model):
-    id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=50)
-    descripcion = models.TextField()
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='grupos')
-    numero_miembros = models.IntegerField(default=0)
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='grupos')
-
-    def __str__(self):
-        return self.nombre
-
-    class Meta:
-        db_table = 'grupos'
-
-
-class Comentario(models.Model):
-    id = models.AutoField(primary_key=True)
-    titulo = models.CharField(max_length=50)
-    comentario = models.TextField()
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='comentarios')
-    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE, related_name='comentarios')
+class Group(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user_limit = models.IntegerField(default=0)
+    users = models.ManyToManyField(User)
     created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.titulo} - {self.comentario[:30]}..."  
-
-    class Meta:
-        db_table = 'comentarios'
+    updated_at = models.DateTimeField(auto_now=True)
 
 
-class GrupoUsuario(models.Model):
-    id = models.AutoField(primary_key=True)
-    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE, related_name='grupo_usuario')
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='grupo_usuario')
+class Comment(models.Model):
+    desription = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f"Grupo: {self.grupo.nombre}, Usuario: {self.usuario.nombre}"
-
-    class Meta:
-        db_table = 'grupo_usuario'
